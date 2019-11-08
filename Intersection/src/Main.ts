c5ff92cb8cfdcb93cad53c2c4898c41dcd7fc6fb
lkczy <lkczy@users.noreply.github.com>
2019-11-08 11:54:15 +0800

    随机算法优化

--- Intersection/src/Main.ts
+++ Intersection/src/Main.ts
@@ -1,7 +1,7 @@
 import GameConfig from "./GameConfig";
 class Main {
 
-	private tempArr:Array<number>;
+	private tempArr: Array<number>;
 
 	constructor() {
 		//根据IDE设置初始化引擎		
@@ -13,85 +13,90 @@ class Main {
 		Laya.stage.screenMode = GameConfig.screenMode;
 		Laya.stage.alignV = GameConfig.alignV;
 		Laya.stage.alignH = GameConfig.alignH;
-		Laya.stage.bgColor="#000000";
+		Laya.stage.bgColor = "#000000";
 
 		//生成数组
-		let arr1:Array<number>=new Array<number>(1000);
-		let arr2:Array<number>=new Array<number>(1000);
+		let arr1: Array<number> = new Array<number>();
+		let arr2: Array<number> = new Array<number>();
 
 		//将生成的随机数push进两个数组
-		this.tempArr=new Array<number>(10001);
-		for(let i=0;i<1000;i++){
-			arr1[i]=this.createrRandomNumber();
-		}
-		this.tempArr=new Array<number>(10001);
-		for(let i=0;i<1000;i++){
-			arr2[i]=this.createrRandomNumber();
-		}
+		this.tempArr = this.summonNumArr();
+		arr1 = this.createRandomNum(this.tempArr);
 
-		let arr3:Array<number>=this.getIntersection(arr1,arr2);		
+		this.tempArr = this.summonNumArr();
+		arr2 = this.createRandomNum(this.tempArr);
+
+
+		let arr3: Array<number> = this.getIntersection(arr1, arr2);
 
 		//打印到屏幕
-		let printTxt:Laya.Text=new Laya.Text();
+		let printTxt: Laya.Text = new Laya.Text();
 		Laya.stage.addChild(printTxt);
-		printTxt.pos(50,10);
-		printTxt.fontSize=25;
-		printTxt.color="#0fffff";
+		printTxt.pos(50, 10);
+		printTxt.fontSize = 25;
+		printTxt.color = "#0fffff";
 
-		let count:number = 0;
-		for(let element of arr3)
-		{
-			if(count<12)
-			{
-				printTxt.text += element.toString()+" ";
+		let count: number = 0;
+		let maxDisplayCountForRow = 12;
+		for (let element of arr3) {
+			if (count < maxDisplayCountForRow) {
+				printTxt.text += element.toString() + " ";
 				count++;
 			}
-			else
-			{
-				count=0;
-				printTxt.text += element.toString()+"\n";
+			else {
+				count = 0;
+				printTxt.text += element.toString() + "\n";
 
-			}			
-		}
-
-
-
-
-	}
-
-	
-	//生成随机数
-	private createrRandomNumber():number{
-		let randomNum:number;
-		while(true){
-			randomNum=Math.round(Math.random()*10000);
-			if(this.tempArr[randomNum]==undefined){
-				this.tempArr[randomNum]=randomNum;
-				break;
 			}
 		}
-		return randomNum;
 	}
-	
+
+
+	//生成一个有10000个数的数组
+	private summonNumArr(): Array<number> {
+		let numArr = new Array<number>();
+		let numArrLength = 10000;
+		for (let i = 0; i < numArrLength; i++) {
+			numArr.push(i);
+		}
+		return numArr;
+	}
+
+	//洗牌算法
+	private createRandomNum(numArr: Array<number>): Array<number> {
+		for (let i = numArr.length - 1; i > 0; i--) {//打乱导入数组的顺序
+			let j: number = Math.floor(Math.random() * (i + 1));
+			let temp = numArr[i];
+			numArr[i] = numArr[j];
+			numArr[j] = temp;
+		}
+
+		let returnArrLength = 1000;
+		let randomArr = new Array<number>();
+		for (let i = 0; i < returnArrLength; i++) {
+			randomArr[i] = numArr[i];
+		}
+		return randomArr;
+	}
+
 
 	//对比两个数组
-	private getIntersection(arr1:Array<number>,arr2:Array<number>):Array<number>{
-		let arr3:Array<number>=new Array<number>();
-		let map:{[key:number]:number}={};
-		for(let i=0;i<arr1.length;i++){
-			map[arr1[i]]=1;
+	private getIntersection(arr1: Array<number>, arr2: Array<number>): Array<number> {
+		let arr3: Array<number> = new Array<number>();
+		let queryData = new Array<number>(10000);
+		let map: { [key: number]: number } = {};
+		for (let i = 0; i < arr1.length; i++) {
+			map[arr1[i]] = 1;
+			queryData[i] = 1;
 		}
-		for(let i=0;i<arr2.length;i++){
-			if(map[arr2[i]]>0){
-				map[arr2[i]]=0;
+		for (let i = 0; i < arr2.length; i++) {
+			if (map[arr2[i]] > 0) {
+				map[arr2[i]] = 0;
 				arr3.push(arr2[i]);
 			}
 		}
-		return	arr3;
+		return arr3;
 	}
-
-
-
 }
 //激活启动类
 new Main();
